1a11907ee40048fbf21063f716675dc289bfaebc
Vũ Hoài Nam <vunam.cmg@gmail.com>
2019-05-13 10:50:59 +0700

    calculator score for user who write post when post reacted by another user

--- src/controllers/crud/postReaction/postReaction.controller.ts
+++ src/controllers/crud/postReaction/postReaction.controller.ts
@@ -1,10 +1,33 @@
 import { CrudController } from '../../crudController'
-import { postReactionService } from '../../../services/crud/postReaction'
+import { postReactionService, postReactionErrorService } from '../../../services/crud/postReaction'
+import { ICrudOption } from '../../../services/crudService';
+import { userService } from '../../../services/crud/user';
+
+import { sequelize } from '../../../models';
 
 
 export class PostReactionController extends CrudController<typeof postReactionService>{
-    constructor(){
+    constructor() {
         super(postReactionService);
     }
-    
+    async create(params: any, option?: ICrudOption) {
+        await userService.increaseScore(params.postAuthorId)
+        return await this.service.create(params, option)
+    }
+    async delete(option?: ICrudOption) {
+       // return await this.service.delete(option)
+        let transaction = await sequelize.transaction()
+        try {
+            const postReaction = await this.service.model.findOne({ where: option.filter }, transaction)
+            await userService.decreaseScore(postReaction.postAuthorId, transaction)
+            await this.service.model.destroy({ where: option.filter }, transaction)
+            await transaction.commit()
+            return postReaction
+        } catch (err) {
+            console.log("err: ", err)
+            await transaction.rollback()
+            throw postReactionErrorService.cantUnreactionPost()
+        }
+    }
+
 }
