fdb59d8900789927c1e4c05f5a473f87e82daf61
Vũ Hoài Nam <vunam.cmg@gmail.com>
2019-07-27 15:01:42 +0700

    tracking comment to notifi to post owner user

--- src/controllers/crud/comment/comment.controller.ts
+++ src/controllers/crud/comment/comment.controller.ts
@@ -1,10 +1,28 @@
 import { CrudController } from '../../crudController'
 import { commentService } from '../../../services/crud/comment'
+import { ICrudOption, notificationSystemService } from '../../../services';
+import { userService } from '../../../services/crud/user';
+import { postService } from '../../../services/crud/post';
 
 
 export class CommentController extends CrudController<typeof commentService>{
-    constructor(){
+    constructor() {
         super(commentService);
     }
-    
+    async create(params: any, option?: ICrudOption) {
+        const result = await this.service.create(params, option)
+        postService.getItem({ filter: { _id: params.postId } }).then(result => {
+            if(result.get("userId") === params.userId) return
+            notificationSystemService.tracking({
+                type: "comment_on_post",
+                toUserId: result.get("userId"),
+                fromUserId: params.userId,
+                payload: {
+                    content: params.content
+                }
+            })
+        })
+
+        return result
+    }
 }
